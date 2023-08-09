4ff657ac1e0fa79091516001acc3667688e01014
Home <home@homes-mbp.lan>
2023-08-08 22:56:41 -0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "math.h"
+
+int main() {
+  using namespace std;
+
+  int x = 1;
+  int y = 2;
+  int result = Add(x, y);
+  std::cout << 4 << std::endl;
+  return 0;
+}
