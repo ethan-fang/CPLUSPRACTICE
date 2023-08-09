a7df43d1d0836bd2b3b0f8e661d554842616cda8
Home <home@homes-mbp.lan>
2023-08-08 23:40:20 -0700

    added pointer

--- main.cpp
+++ main.cpp
@@ -1,17 +1,10 @@
 #include <iostream>
 #include "math.h"
 
-int main() {
-  using namespace std;
+using namespace std;
 
-  int x = 1;
-  int y = 2;
-  int result = Add(x, y);
-  std::cout << 4 << std::endl;
-  return 0;
-}
-
-void initiateValues() {
+void initiateValues()
+{
   int a1;
   int a3(5);
   std::string s1("C++");
@@ -35,8 +28,41 @@ void initiateValues() {
    * Value initialization => T obj{};
    * Direct initialization => T obj{v};
    * Copy initialization => T obj = v;
-  */
+   */
 
- float f{};
-//  int i{f};
+  float f{};
+  //  int i{f};
+}
+
+void addressOperator()
+{
+  int x = 10;
+  int *ptr = &x;
+  std::cout << &x << "\n";
+  std::cout << ptr << "\n";
+
+
+  *ptr = 5;
+  std::cout << x << "\n";
+
+  int y = *ptr;
+  std::cout << y << "\n";
+
+  int *newPtr = nullptr;
+
+  // *newPtr = 10;
+  cout << newPtr << "\n";
+}
+
+int main()
+{
+  using namespace std;
+
+  int x = 1;
+  int y = 2;
+  int result = Add(x, y);
+  std::cout << 4 << std::endl;
+
+  addressOperator();
+  return 0;
 }
