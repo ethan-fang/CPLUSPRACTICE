af67249b48cfc6b18a6469fcad15de53ba904612
Home <home@homes-mbp.lan>
2023-08-10 08:00:49 -0700

    added 2D array

--- memory_management/main.cpp
+++ memory_management/main.cpp
@@ -32,6 +32,28 @@ void newArray()
   delete[] p;
 }
 
+void new2DArray()
+{
+  int data[2][3] = {
+    1, 2, 3,
+    4, 5, 6,
+  }
+
+  int *p1 = new int[3];
+  int *p2 = new int[3];
+
+  int **pData = new int *[2];
+  pData[0] = p1;
+  pData[1] = p2;
+
+  pData[0][1] = 2;
+
+  delete[]p1;
+  delete[]p2;
+
+  delete [] pData;
+}
+
 void newString()
 {
   char *p = new char[5];
