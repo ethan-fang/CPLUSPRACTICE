7cffb6cd11c0a662456957cc78fa78f7de4149c6
Home <home@homes-mbp.lan>
2023-08-10 07:44:47 -0700

    added new[] operator

--- memory_management/main.cpp
+++ memory_management/main.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
+#include <string.h>
 
 void dynamicMemory()
 {
@@ -12,15 +13,36 @@ void dynamicMemory()
   free(p);
 }
 
-void newOperator() {
+void newOperator()
+{
   int *p = new int;
   *p = 6;
   std::cout << *p << std::endl;
   delete p;
   p = nullptr;
+}
+
+void newArray()
+{
+  int *p = new int[5];
+  for (int i = 0; i < 5; ++i)
+  {
+    p[i] = i;
+  }
+  delete[] p;
+}
+
+void newString()
+{
+  char *p = new char[5];
+  strncpy(p, "C++", 4);
+  std::cout << p << std::endl;
+  delete[] p;
+}
 
 int main()
 {
   // dynamicMemory();
-  newOperator();
+  // newOperator();
+  newString();
 }
